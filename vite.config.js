230de67e8e52602f1f0965ce665b4a596b174d4e
unknown <jouga@jojonomic.com>
2024-10-06 19:37:19 +0700

    change route and base

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue'
-import SVGLoader from 'vite-svg-loader'
+  import { defineConfig } from 'vite'
+  import { resolve } from 'path'
+  import vue from '@vitejs/plugin-vue'
+  import SVGLoader from 'vite-svg-loader'
 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), SVGLoader()],
-  define: {
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '~@': resolve(__dirname, "/src"),
-    }
-  },
-  server: {
-    host: true
-  },
-  base: '/wedding-invitation/'
-})
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    plugins: [vue(), SVGLoader()],
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '~@': resolve(__dirname, "/src"),
+      }
+    },
+    server: {
+      host: true
+    },
+    base: '/'
+  })
